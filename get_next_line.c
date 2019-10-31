f8dd7c5b425b1d26333d130d6318bebb9b121013
Baobe <wmmxxbb@gmail.com>
2019-10-31 16:55:43 +0100

    original version

---
+++ get_next_line.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 3200
+
+int          ft_strlen(char *str)
+{
+	int  i;
+	int  lens;
+
+	i = -1;
+	lens = 0;
+	if (str)
+		while (str[++i])
+			lens++;
+	return (lens);
+}
+
+int no_newline_in_str(char *str)
+{
+	int i;
+
+	i = 0;
+	if (!str)
+		return (1);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char *get_content_fron_file(int fd, char *str)
+{
+	int nb_read;
+	char *s;
+	char buff[BUFFER_SIZE + 1];
+
+	buff[BUFFER_SIZE] = '\0';
+	nb_read = read(fd, buff, BUFFER_SIZE);
+	buff[nb_read] = '\0';
+	if (nb_read > 0)
+	{
+		s =(char *)malloc(sizeof(char) * (ft_strlen(str) + nb_read + 1));
+		if (!s)
+			return (NULL);
+		if (str)
+			s = strcat(s, str);
+		s = strncat(s, buff, nb_read);
+		if (no_newline_in_str(s))
+			return (get_content_fron_file(fd, s));
+		return (s);
+	}
+	return (NULL);
+}
+
+char *extract_line(char *str)
+{
+	char *s;
+	int i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!(s = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	s[i] = '\0';
+	s = strncpy(s, str, i);
+	return (s);
+}
+
+char *remove_str(char *str)
+{
+	int i;
+	int j;
+	char *s;
+
+	i = 0;
+	j = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	j = ft_strlen(str) - i;
+	s = (char *)malloc(sizeof(char) * j);
+	s[--j] = '\0';
+	i = ft_strlen(str);
+	while (--j >= 0)
+		s[j] = str[--i];
+	return (s);
+}
+
+int get_next_line(int fd, char **line)
+{
+	static char *str = NULL;
+
+	if (no_newline_in_str(str))
+		if (!(str = get_content_fron_file(fd, str)))
+			return (-1);
+	*line = extract_line(str) ;
+	str = remove_str(str);
+	printf("%s\n", *line);
+}
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	char *str = NULL;
+	int fd = open(av[1],O_RDONLY);
+	char *line;
+	line = NULL;
+	get_next_line(fd, &line);
+	return (0);
+
+}
